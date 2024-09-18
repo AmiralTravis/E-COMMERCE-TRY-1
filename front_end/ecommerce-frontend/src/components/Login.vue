e4da904b5126b74db440ec73b828d1bba7f16c80
AmiralTravis <asbia530@gmail.com>
2024-09-18 22:33:02 +0530

    log in and sign up properly done

--- front_end/ecommerce-frontend/src/components/Login.vue
+++ front_end/ecommerce-frontend/src/components/Login.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="login">
       <h1>Log In</h1>
       <form @submit.prevent="submitLoginForm">
@@ -11,9 +11,9 @@
           <input type="password" v-model="password" id="password" required />
         </div>
         <button type="submit">Log In</button>
-      </form>
+      </form> -->
       <!-- Display a message based on success or failure -->
-      <p v-if="successMessage" style="color: green">{{ successMessage }}</p>
+      <!-- <p v-if="successMessage" style="color: green">{{ successMessage }}</p>
       <p v-if="errorMessage" style="color: red">{{ errorMessage }}</p>
       <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
     </div>
@@ -68,6 +68,98 @@
   
   button {
     margin-top: 10px;
+  }
+  </style>
+   -->
+
+   <template>
+    <div class="modal">
+      <div class="modal-content">
+        <span class="close" @click="$emit('update:visible', false)">&times;</span>
+        <h1>Log In</h1>
+        <form @submit.prevent="submitLoginForm">
+          <div>
+            <label for="username">Username:</label>
+            <input type="text" v-model="username" id="username" required />
+          </div>
+          <div>
+            <label for="password">Password:</label>
+            <input type="password" v-model="password" id="password" required />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+        <p v-if="successMessage" style="color: green">{{ successMessage }}</p>
+        <p v-if="errorMessage" style="color: red">{{ errorMessage }}</p>
+        <p>Don't have an account? <router-link to="/signup">Sign up</router-link></p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        successMessage: '',
+        errorMessage: ''
+      };
+    },
+    methods: {
+      async submitLoginForm() {
+        try {
+          const loginData = {
+            username: this.username,
+            password: this.password
+          };
+  
+          const response = await axios.post('http://localhost:5000/api/auth/login', loginData);
+  
+          if (response.status === 200) {
+            this.successMessage = 'Login successful!';
+            this.errorMessage = '';
+          }
+        } catch (error) {
+          this.errorMessage = 'Error logging in. Please try again.';
+          this.successMessage = '';
+        }
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .modal {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+  
+  .modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 30%;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+  }
+  
+  .close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  
+  .close:hover {
+    color: black;
   }
   </style>
   
