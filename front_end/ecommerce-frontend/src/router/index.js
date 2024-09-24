60f721fd5f967187488cec42f4121ff84635a4d7
AmiralTravis <asbia530@gmail.com>
2024-09-24 12:28:33 +0530

    products are being loaded properly, products loading part done

--- front_end/ecommerce-frontend/src/router/index.js
+++ front_end/ecommerce-frontend/src/router/index.js
@@ -1,4 +1,34 @@
+// import { createRouter, createWebHistory } from 'vue-router';
+
+// // Import components that will be linked to each route
+// import HomeFrontend from '../components/HomeFrontend.vue';
+// import ProductList from '../views/ProductList.vue';
+// import ProductDetails from '../views/ProductDetails.vue';
+// import CartFrontend from '../views/CartFrontend.vue';
+// import CheckoutFrontend from '../components/CheckoutFrontend.vue';
+// import SignUp from '../components/SignUp.vue'; // Import the SignUp component
+// import Login from '../components/LogIn.vue'; // Import the Login component
+
+// const routes = [
+//   { path: '/', name: 'Home', component: HomeFrontend },
+//   { path: '/products', name: 'ProductList', component: ProductList },
+//   { path: '/products/:id', name: 'ProductDetails', component: ProductDetails },
+//   { path: '/cart', name: 'Cart', component: CartFrontend },
+//   { path: '/checkout', name: 'Checkout', component: CheckoutFrontend },
+//   { path: '/signup', name: 'SignUp', component: SignUp }, // Add the signup route
+//   { path: '/login', name: 'Login', component: Login }, // Add the login route
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+
+// export default router;
+
+
 import { createRouter, createWebHistory } from 'vue-router';
+import store from '../store/index.js'; // Updated path to store
 
 // Import components that will be linked to each route
 import HomeFrontend from '../components/HomeFrontend.vue';
@@ -6,17 +36,24 @@ import ProductList from '../views/ProductList.vue';
 import ProductDetails from '../views/ProductDetails.vue';
 import CartFrontend from '../views/CartFrontend.vue';
 import CheckoutFrontend from '../components/CheckoutFrontend.vue';
-import SignUp from '../components/SignUp.vue'; // Import the SignUp component
-import Login from '../components/LogIn.vue'; // Import the Login component
+import SignUp from '../components/SignUp.vue';
+import Login from '../components/LogIn.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomeFrontend },
-  { path: '/products', name: 'ProductList', component: ProductList },
+  { 
+    path: '/products', 
+    name: 'ProductList', 
+    component: ProductList,
+    beforeEnter: (to, from, next) => {
+      store.dispatch('products/fetchProducts').then(() => next());
+    },
+  },
   { path: '/products/:id', name: 'ProductDetails', component: ProductDetails },
   { path: '/cart', name: 'Cart', component: CartFrontend },
   { path: '/checkout', name: 'Checkout', component: CheckoutFrontend },
-  { path: '/signup', name: 'SignUp', component: SignUp }, // Add the signup route
-  { path: '/login', name: 'Login', component: Login }, // Add the login route
+  { path: '/signup', name: 'SignUp', component: SignUp },
+  { path: '/login', name: 'Login', component: Login },
 ];
 
 const router = createRouter({
