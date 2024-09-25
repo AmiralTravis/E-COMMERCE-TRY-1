da61f16a237fc194dfeb1e27994a499558284cf3
AmiralTravis <asbia530@gmail.com>
2024-09-25 08:35:16 +0530

    functional cart, all in real time, cart completely done

--- front_end/ecommerce-frontend/src/store/modules/cart.js
+++ front_end/ecommerce-frontend/src/store/modules/cart.js
@@ -1,82 +1,8 @@
-// const state = {
+// import { reactive } from 'vue'
+
+// const state = reactive({
 //   cartItems: []
-// };
-
-// const mutations = {
-//   ADD_TO_CART(state, product) {
-//     const existingItem = state.cartItems.find(item => item.id === product.id);
-//     if (existingItem) {
-//       existingItem.quantity++;
-//     } else {
-//       state.cartItems.push({ ...product, quantity: 1 });
-//     }
-//   },
-//   REMOVE_FROM_CART(state, productId) {
-//     state.cartItems = state.cartItems.filter(item => item.id !== productId);
-//   },
-//   UPDATE_CART_QUANTITY(state, { productId, quantity }) {
-//     const item = state.cartItems.find(item => item.id === productId);
-//     if (item) {
-//       // Use Vue.set or reassign to ensure reactivity
-//       item.quantity = quantity;
-//     }
-//   }
-// };
-
-// const actions = {
-//   addToCart({ commit }, product) {
-//     commit('ADD_TO_CART', product);
-//   },
-//   removeFromCart({ commit }, productId) {
-//     commit('REMOVE_FROM_CART', productId);
-//   },
-//   updateCartQuantity({ commit }, { productId, quantity }) {
-//     commit('UPDATE_CART_QUANTITY', { productId, quantity });
-//   }
-// };
-
-// const getters = {
-//   cartItems: (state) => state.cartItems,
-//   cartTotalPrice: (state) =>
-//     state.cartItems.reduce((total, item) => total + item.price * item.quantity, 0),
-// };
-
-// export default {
-//   namespaced: true,
-//   state,
-//   mutations,
-//   actions,
-//   getters,
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const state = {
-//   cartItems: []
-// };
+// });
 
 // const mutations = {
 //   ADD_TO_CART(state, product) {
@@ -151,8 +77,11 @@ const mutations = {
   UPDATE_CART_QUANTITY(state, { productId, quantity }) {
     const item = state.cartItems.find(item => item.id === productId);
     if (item) {
-      item.quantity = quantity;
+      item.quantity = parseInt(quantity);
     }
+  },
+  CLEAR_CART(state) {
+    state.cartItems = [];
   }
 };
 
@@ -165,6 +94,9 @@ const actions = {
   },
   updateCartQuantity({ commit }, { productId, quantity }) {
     commit('UPDATE_CART_QUANTITY', { productId, quantity });
+  },
+  clearCart({ commit }) {
+    commit('CLEAR_CART');
   }
 };
 
@@ -172,6 +104,8 @@ const getters = {
   cartItems: (state) => state.cartItems,
   cartTotalPrice: (state) =>
     state.cartItems.reduce((total, item) => total + item.price * item.quantity, 0),
+  cartItemCount: (state) =>
+    state.cartItems.reduce((count, item) => count + item.quantity, 0),
 };
 
 export default {
